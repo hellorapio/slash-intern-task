f8c13568b14bacb4709b8a54cf57595ecf363c0d
Anubis <anubis356@outlook.com>
2024-06-19 14:52:58 +0300

    add: Config Module and Prisma Schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { CartController } from "./modules/cart/cart.controller";
+import { UsersController } from "./modules/users/users.controller";
+import { OrdersController } from "./modules/orders/orders.controller";
+import { ProductsController } from "./modules/products/products.controller";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env.local",
+    }),
+  ],
+  controllers: [
+    AppController,
+    CartController,
+    UsersController,
+    OrdersController,
+    ProductsController,
+  ],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  app.setGlobalPrefix("/api/", { exclude: ["health"] });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      disableErrorMessages: process.env.NODE_ENV === "production",
+    })
+  );
+
+  await app.listen(config.get("PORT"));
 }
 bootstrap();
